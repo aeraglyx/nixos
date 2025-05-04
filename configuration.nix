6e779656213be2b85638e0a06647a87651220467
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-05-04 04:04:47 +0200

    Backup

--- configuration.nix
+++ configuration.nix
@@ -1,19 +1,97 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-    imports = [
-        <nixos-wsl/modules>
-    ];
+    imports =
+        [
+            ./hardware-configuration.nix
+        ];
 
-    wsl.enable = true;
-    wsl.defaultUser = "aeraglyx";
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "nixos"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    networking.networkmanager.enable = true;
 
     time.timeZone = "Europe/Prague";
 
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+    };
+
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    services.xserver.displayManager.gdm.enable = true;
+    services.xserver.desktopManager.gnome.enable = true;
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+    };
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound with pipewire.
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        # jack.enable = true;
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        # media-session.enable = true;
+    };
+
     users.users.aeraglyx = {
         isNormalUser = true;
-        extraGroups = [ "wheel" ];
+        description = "aeraglyx";
+        extraGroups = [ "networkmanager" "wheel" ];
     };
+
+    nixpkgs.config.allowUnfree = true;
+
+    programs.hyprland = {
+        enable = true;
+        # nvidiaPatches = true;
+        xwayland.enable = true;
+    };
+
+    # environment.sessionVariables = {
+    #   WLR_NO_HARDWARE_CURSORS = "1";
+    #   NIXOS_OXONE_WL = "1";
+    # };
+    #
+    # hardware = {
+    #   opengl.enable = true;
+    #   nvidia.modesetting.enable = true;
+    # };
+
+    xdg.portal.enable = true;
+    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+    programs.firefox.enable = true;
 
     environment.systemPackages = with pkgs; [
         git
@@ -23,6 +101,12 @@
         fzf
         lazygit
         ripgrep
+
+        waybar
+        rofi-wayland
+        hyprpaper
+        wl-clipboard
+        cliphist
 
         tmux
         kitty
@@ -37,9 +121,39 @@
         lua-language-server
 
         qmk
+        # spotify
     ];
 
+    environment.variables.SUDO_EDITOR = "nvim";
     environment.variables.EDITOR = "nvim";
+    environment.variables.VISUAL = "nvim";
+
+    fonts.packages = with pkgs; [
+        # TODO refactor after 25.05
+        (nerdfonts.override { fonts = [ "FiraCode" ]; })
+        fira-code
+        font-awesome
+    ];
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
+    #
+    # List services that you want to enable:
+    #
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
+    #
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
     system.stateVersion = "24.11";
+
 }
