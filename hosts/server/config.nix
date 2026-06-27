a215dddcae18b9fe6d574f1160ebc4854d08e0f7
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-06-27 03:07:11 +0200

    nixos: update server config

--- hosts/server/config.nix
+++ hosts/server/config.nix
@@ -17,13 +17,20 @@
     };
 
     boot.loader.systemd-boot.enable = true;
+    boot.loader.systemd-boot.configurationLimit = 4;
     boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.timeout = 0;
 
     networking.hostName = "server";
     networking.networkmanager.enable = true;
     networking.wireless.enable = true;
 
+    services.resolved.enable = true;
     services.openssh.enable = true;
+
+    environment.systemPackages = with pkgs; [
+        brightnessctl
+    ];
 
     system.stateVersion = "26.05";
 }
