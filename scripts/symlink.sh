5af46bef9ce8536ed1cfec6d76aba7e9b241655c
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-06-28 05:30:31 +0200

    scripts: update symlinking

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,13 +1,29 @@
 dot_src=~/nixos/dotfiles
 
+BLACK='\033[30m'
+RED='\033[31m'
+RESET='\033[0m'
+
 symlink () {
     src=$1
     dst=$2
     if [ ! -e "$src" ]; then
-        echo "missing src $src"
+        echo -e "${RED}[ missing src ] $src${RESET}"
+    elif [ -L "$dst" ] && [ "$src" -ef "$dst" ]; then
+        echo -e "${BLACK}[ skipped dst ] $dst${RESET}"
     else
-        echo "created dst $dst"
-        ln -sfn "$src" "$dst"
+        mkdir -p "${dst%/*}"
+        if [ -e "$dst" ]; then
+            echo -n "[ overwrite ? ] $dst "
+            read -r -s -N 1 char && echo
+            if [ "$char" = $'\n' ]; then
+                rm -rf "$dst"
+                ln -sfn "$src" "$dst"
+            fi
+        else
+            echo "[ created dst ] $dst"
+            ln -sfn "$src" "$dst"
+        fi
     fi
 }
 
@@ -23,7 +39,7 @@ symlink_conf () {
     symlink "$src" "$dst"
 }
 
-programs=(alacritty beets btop direnv dunst eza fastfetch flameshot fzf ghostty git hypr kitty lazygit mpv nvim qmk qutebrowser rofi starship sunsetr tealdeer tmux waybar yazi)
+programs=(alacritty beets btop direnv dunst eza fastfetch flameshot fzf ghostty git hypr kitty lazygit mpv nvim qmk qutebrowser rmpc rofi starship sunsetr tealdeer tmux waybar yazi)
 
 for program in "${programs[@]}"; do
     symlink_conf "$program"
@@ -32,3 +48,4 @@ done
 symlink_home "bash/.bashrc" ".bashrc"
 symlink_home "bash/.profile" ".profile"
 symlink_home "ssh/config" ".ssh/config"
+symlink_home "blender/scripts/startup" ".config/blender/5.1/scripts/startup"
