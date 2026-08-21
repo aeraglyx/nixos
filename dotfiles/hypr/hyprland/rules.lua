e8cf2bcc94dfd073f5b2e3ed7f088ef296cd820f
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-08-21 13:43:23 +0200

    hyprland: update rules

--- dotfiles/hypr/hyprland/rules.lua
+++ dotfiles/hypr/hyprland/rules.lua
@@ -1,50 +1,49 @@
 -- https://wiki.hypr.land/Configuring/Basics/Window-Rules/
 -- https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 
-hl.window_rule({ match = { title = ".*Blender.*" },   workspace = "4" })
-hl.window_rule({ match = { title = ".*Discord.*" },   workspace = "5" })
-hl.window_rule({ match = { title = ".*Signal.*" },    workspace = "5" })
-hl.window_rule({ match = { title = ".*Moonlight.*" }, workspace = "9" })
-hl.window_rule({ match = { title = ".*Parsec.*" },    workspace = "9" })
+local colors = require("hyprland.theme")
 
 
 local size_wide = { "monitor_w * 0.5", "monitor_h * 0.5" }
 local size_high = { "monitor_w * 0.3", "monitor_h * 0.8" }
 local size_thic = { "monitor_w * 0.6", "monitor_h * 0.7" }
 
-
-local function class_floating_win(class, size)
+local function match_floating_win(match, size)
     hl.window_rule({
-        match = { class = class },
+        match = match,
         size = size,
         float = true,
         center = true,
     })
 end
 
-class_floating_win("popup.app.wide", size_wide)
-class_floating_win("popup.app.high", size_high)
-class_floating_win("popup.app.thic", size_thic)
+
+hl.window_rule({ match = { title = ".*Blender.*" },   workspace = "4" })
+hl.window_rule({ match = { title = ".*Discord.*" },   workspace = "5" })
+hl.window_rule({ match = { title = ".*Signal.*" },    workspace = "5" })
+hl.window_rule({ match = { title = ".*Moonlight.*" }, workspace = "9" })
+hl.window_rule({ match = { title = ".*Parsec.*" },    workspace = "9" })
+hl.window_rule({ match = { title = ".*Tor Browser.*" }, workspace = "10" })
 
 
-local function blender_floating_win(title, size)
-    hl.window_rule({
-        match = { class = "blender", title = title },
-        size = size,
-        float = true,
-        center = true,
-    })
-end
-
-blender_floating_win("Preferences", size_high)
-blender_floating_win("File Browser", size_wide)
-blender_floating_win("Image Editor", size_wide)
+hl.window_rule({ match = { class = "firefox" }, scrolling_width = 0.7})
+hl.window_rule({ match = { class = "blender" }, scrolling_width = 0.8})
 
 
--- moonlight
+match_floating_win({ class = "popup.app.wide" }, size_wide)
+match_floating_win({ class = "popup.app.high" }, size_high)
+match_floating_win({ class = "popup.app.thic" }, size_thic)
+
+match_floating_win({ class = "blender", title = "Preferences" }, size_high)
+match_floating_win({ class = "blender", title = "File Browser" }, size_wide)
+match_floating_win({ class = "blender", title = "Image Editor" }, size_wide)
+
+match_floating_win({ class = "xdg-desktop-portal-gtk" }, size_wide)
+match_floating_win({ class = "qimgv", title = "Edit shortcut" }, size_wide)
+
+
+hl.window_rule({ match = { class = "Matplotlib" }, float = true, center = true })
 hl.window_rule({ match = { title = ".* - Moonlight.*" }, fullscreen = true })
-
--- showmethekey
 hl.window_rule({ match = { class = "one.alynx.showmethekey" }, float = true })
 hl.window_rule({
     match = { class = "showmethekey-gtk" },
@@ -55,14 +54,10 @@ hl.window_rule({
     size = { 600, 80 }
 })
 
-hl.window_rule({ match = { class = "firefox" }, scrolling_width = 0.7})
-hl.window_rule({ match = { class = "blender" }, scrolling_width = 0.8})
 
--- matplotlib
-hl.window_rule({ match = { class = "Matplotlib" }, float = true, center = true })
-
--- no shadow for tiled windows
+hl.window_rule({ match = { class = "Tor Browser" }, border_color = colors.red })
 hl.window_rule({ match = { float = false }, no_shadow = true })
+
 
 -- ignore maximize requests from all apps
 hl.window_rule({
