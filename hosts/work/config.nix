744ef4ba27254714eea3b9569ea0d95e075fc45e
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-08-21 12:58:01 +0200

    nixos: cleanup work host

--- hosts/work/config.nix
+++ hosts/work/config.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, pkgs-unstable, ... } @ inputs:
+{ config, pkgs, pkgs-unstable }:
 
 {
     nixpkgs.hostPlatform = "x86_64-linux";
@@ -9,10 +9,4 @@
         enable = true;
         defaultUser = "aeraglyx";
     };
-
-    # environment.systemPackages = with pkgs-unstable; [
-    #     blender_4_5
-    #     qmk
-    # ];
 }
-
