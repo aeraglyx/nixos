99237a26b6e4a96679857c0d7237555fa0d46190
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-08-21 12:57:12 +0200

    nixos: laptop hw formatting

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -10,16 +10,16 @@
     boot.kernelModules = [ "kvm-intel" ];
     boot.extraModulePackages = [ ];
 
-    fileSystems."/" =
-        { device = "/dev/disk/by-uuid/25022132-44c3-4ba7-83f1-41c1df240224";
-            fsType = "ext4";
-        };
+    fileSystems."/" = {
+        device = "/dev/disk/by-uuid/25022132-44c3-4ba7-83f1-41c1df240224";
+        fsType = "ext4";
+    };
 
-    fileSystems."/boot" =
-        { device = "/dev/disk/by-uuid/F8FD-0C31";
-            fsType = "vfat";
-            options = [ "fmask=0022" "dmask=0022" ];
-        };
+    fileSystems."/boot" = {
+        device = "/dev/disk/by-uuid/F8FD-0C31";
+        fsType = "vfat";
+        options = [ "fmask=0022" "dmask=0022" ];
+    };
 
     swapDevices = [ ];
 
