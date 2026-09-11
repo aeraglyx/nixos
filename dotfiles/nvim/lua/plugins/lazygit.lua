84486f08659fd2ed90496ae686206f7049d4b7c9
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-09-11 14:15:57 +0200

    nvim: fix wrong plenary url

--- dotfiles/nvim/lua/plugins/lazygit.lua
+++ dotfiles/nvim/lua/plugins/lazygit.lua
@@ -1,6 +1,6 @@
 vim.pack.add({
     "https://github.com/kdheepak/lazygit.nvim",
-    "https://github.com/nvim-tree/nvim-lua/plenary.nvim"
+    "https://github.com/nvim-lua/plenary.nvim"
 })
 
 vim.g.lazygit_floating_window_scaling_factor = 1.0
