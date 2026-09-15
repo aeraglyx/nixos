9c720cf3c6459479e7242a4784bc0bd1a858e273
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-09-15 09:21:06 +0200

    nvim: don't open yazi for dirs

--- dotfiles/nvim/lua/plugins/yazi.lua
+++ dotfiles/nvim/lua/plugins/yazi.lua
@@ -6,7 +6,6 @@ vim.pack.add({
 local yazi = require("yazi")
 
 yazi.setup({
-    open_for_directories = true,
     floating_window_scaling_factor = 0.8,
     keymaps = {
         show_help = "<f1>",
