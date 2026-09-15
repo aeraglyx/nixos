8db79258846c58338172079ab2cdf3d38dec65da
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2026-09-15 09:57:48 +0200

    nvim: fix condition for onyx

--- dotfiles/nvim/lua/plugins/onyx.lua
+++ dotfiles/nvim/lua/plugins/onyx.lua
@@ -1,6 +1,6 @@
 local path = vim.fn.expand("~/projects/code/onyx.nvim/")
 
-if vim.fn.isdirectory(path) then
+if vim.fn.isdirectory(path) == 1 then
     vim.opt.rtp:append(path)
 else
     vim.pack.add({ "https://github.com/aeraglyx/onyx.nvim" })
